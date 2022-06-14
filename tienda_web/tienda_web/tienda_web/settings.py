0a1d24132e4e071bce27b771d2fb5f79eeaff08a
VeneciaGonzalez <marleneveneciagonzalez@gmail.com>
2022-06-14 18:32:13 -0400

    javascritp

--- tienda_web/tienda_web/tienda_web/settings.py
+++ tienda_web/tienda_web/tienda_web/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 import os
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'tienda/statics'),)
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'tienda/static'),)
 
 
 ####################################################################
